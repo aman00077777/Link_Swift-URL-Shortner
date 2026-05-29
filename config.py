23409ba5e87813cba2ca950c5aeb1d3e27cc44c3
aman00077777 <aman20061203@gmail.com>
2026-05-29 14:55:58 +0530

    migrate to PostgreSQL

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-# Load env variables from .env file
 load_dotenv()
 
-
 class Config:
-    SECRET_KEY = os.getenv(
-        "FLASK_SECRET_KEY",
-        "default-fallback-key-987654321")
+    SECRET_KEY = os.getenv("FLASK_SECRET_KEY", "default-fallback-key-987654321")
     BASE_URL = os.getenv("BASE_URL", "http://localhost:5000").rstrip("/")
+    DATABASE_URL = os.getenv("DATABASE_URL", None)
     DATABASE_FILE = os.getenv("DATABASE_FILE", "urls.db")
